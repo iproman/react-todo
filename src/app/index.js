652828acd37c5261f43f71de43679f34cc864038
rustamikusyakus <404marao@gmail.com>
2018-07-04 00:51:35 +0300

    Add and fix index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,28 @@
-alert('Hello world');
+/**
+ * @type {React}
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+// Create component
+// var TodoComponent = React.createClass({
+//     render: function () {
+//         return (
+//             <h1>Hello World</h1>
+//         );
+//     }
+// });
+
+var TodoComponent = module.exports=createReactClass({
+   render:function(){
+       return(
+         <div>
+             <h1>Hello world</h1>
+         </div>
+       );
+   }
+});
+
+// Put component into html page
+ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
