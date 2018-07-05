d473695ab0d24e2a7924bf31c48eabf263d91187
rustamikusyakus <404marao@gmail.com>
2018-07-05 22:26:57 +0300

    Changing State and virtual DOM

--- src/app/index.js
+++ src/app/index.js
@@ -35,22 +35,30 @@ var createReactClass = require('create-react-class');
 
 //////////// state
 var TodoComponent = module.exports = createReactClass({
-    getInitialState: function(){
+    getInitialState: function () {
         return {
-            todos: ['wash up', 'eat some cheese', 'take a nap']
-
+            todos: ['wash up', 'eat some cheese', 'take a nap'],
+            age: 30
         }
     },
-    render: function() {
+    render: function () {
+        var ager = setTimeout(function () {
+            this.setState({
+                age: 35,
+
+            })
+        }.bind(this), 5000);
+
         return (
             <div id="todo-list">
                 <p>The busiest people have the most leasure...</p>
+                <p>{this.state.age}</p>
                 <ul>
                     <li>{this.state.todos[0]}</li>
                     <li>{this.state.todos[1]}</li>
                     <li>{this.state.todos[2]}</li>
                 </ul>
-                <listComponent todos={this.state.todos} />
+                <listComponent todos={this.state.todos}/>
             </div>
         );
     } // render
