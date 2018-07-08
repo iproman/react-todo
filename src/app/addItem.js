14fbe600acc94d607d99cea00c08633cdb1a46fb
rustamikusyakus <404marao@gmail.com>
2018-07-08 20:24:57 +0300

    Add handelSubmit

--- src/app/addItem.js
+++ src/app/addItem.js
@@ -4,12 +4,18 @@ var createReactClass = require('create-react-class');
 var AddItem = module.exports = createReactClass({
    render: function () {
        return(
-         <form id="add-todo">
-             <input type="text" required/>
+         <form id="add-todo" onSubmit={this.handleSubmit}>
+             <input type="text" required ref="newItem" />
              <input type="submit" value="Hit me"/>
          </form>
        );
-   }
+   },
+
+   // Custom functions
+    handleSubmit: function (e) {
+       e.preventDefault();
+       this.props.onAdd(this.refs.newItem.value);
+    }
 });
 
 module.exports = AddItem;
