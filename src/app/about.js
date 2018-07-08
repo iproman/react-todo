39d804b36c233f564ce6461aaacf45b5037f6a21
iproman <404marao@gmail.com>
2018-07-08 21:18:35 +0300

    Fix typo

--- src/app/about.js
+++ src/app/about.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var createReactClass = require('create-react-class');
 
-var About = module.exports = React.creaeClass({
+var About = module.exports = React.createClass({
    render: function () {
        return(
            <h2>All about me</h2>
